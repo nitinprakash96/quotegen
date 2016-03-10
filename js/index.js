3a097e5b4de5fcf893b5bd6d42ac2006768fab87
AvengedEndie <prakash.nitin63@gmail.com>
2016-03-10 12:19:53 +0530

    added the API and url for quote generation

--- js/index.js
+++ js/index.js
@@ -11,3 +11,15 @@ var currentQuote = '', currentAuthor = '';
 function openURL(url){
   window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
+function getQuote() {
+  $.ajax({
+    headers: {
+      "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
+      Accept: "application/json",
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
+    success: function(response) {
+      var r = JSON.parse(response);
+      currentQuote = r.quote;
+      currentAuthor = r.author;
