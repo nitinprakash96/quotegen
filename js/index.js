c9a4d54d5ff55075255f167aae93e13b710b1176
AvengedEndie <prakash.nitin63@gmail.com>
2016-03-10 23:19:47 +0530

    index.js completed and reported fine but still looking out for bugs and improvements

--- js/index.js
+++ js/index.js
@@ -24,8 +24,44 @@ function getQuote() {
       currentQuote = r.quote;
       currentAuthor = r.author;
 
-//Cods still to be written.
-//functions to twitter and tumblr
+
+if(inIframe())
+{
+  $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' + encodeURIComponent('"' + currentQuote + '" ' + currentAuthor));
+  $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURIComponent(currentAuthor)+'&content=' + encodeURIComponent(currentQuote));
+}
+$(".quote-text").animate({
+    opacity: 0
+  }, 500,
+  function() {
+    $(this).animate({
+      opacity: 1
+    }, 500);
+    $('#text').text(r.quote);
+  });
+
+  $(".quote-author").animate({
+      opacity: 0
+    }, 500,
+    function() {
+      $(this).animate({
+        opacity: 1
+      }, 500);
+      $('#author').html(r.author);
+    });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $("html body").animate({
+    backgroundColor: colors[color],
+    color: colors[color]
+  }, 1000);
+  $(".button").animate({
+    backgroundColor: colors[color]
+  }, 1000);
+}
+});
+}
+
       $(document).ready(function() {
         getQuote();
         $('#new-quote').on('click', getQuote);
