640732fb8c3a20ef229214c9dcbaa53306a5a779
AvengedEndie <prakash.nitin63@gmail.com>
2016-03-10 11:10:57 +0530

    started working on the core javascript file, namely index.js

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*
+Author: Nitin Prakash
+Email ID: prakash.nitin63@gmail.com
+Github: www.github.com/nitinprakash96
+*/
+
+function inIframe () { try { return window.self !== window.top; } catch (e) { return true; } }
+
+var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857", "#D91F0B", "#4F0BD9"];
+var currentQuote = '', currentAuthor = '';
+function openURL(url){
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+}
