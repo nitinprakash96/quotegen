c44afc1bb24b17b6770e21efc30dc9e41128b30d
AvengedEndie <prakash.nitin63@gmail.com>
2016-03-11 23:19:56 +0530

    removed the URL encoded string as we are using JSON as our POST request.

--- js/index.js
+++ js/index.js
@@ -9,14 +9,14 @@ function inIframe () { try { return window.self !== window.top; } catch (e) { re
 var colors = ['#16a085', '#27ae60', '#2c3e50', '#f39c12', '#e74c3c', '#9b59b6', '#FB6964', '#342224', "#472E32", "#BDBB99", "#77B1A9", "#73A857", "#D91F0B", "#4F0BD9"];
 var currentQuote = '', currentAuthor = '';
 function openURL(url){
-  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=1 ,location=0 ,statusbar=0,menubar=0, resizable=0');
+  window.open(url, 'Share', 'width=550, height=400, toolbar=0, scrollbars=0 ,location=0 ,statusbar=0,menubar=0, resizable=0');
 }
 function getQuote() {
   $.ajax({
     headers: {
       "X-Mashape-Key": "OivH71yd3tmshl9YKzFH7BTzBVRQp1RaKLajsnafgL2aPsfP9V",
       Accept: "application/json",
-      "Content-Type": "application/x-www-form-urlencoded"
+      
     },
     url: 'https://andruxnet-random-famous-quotes.p.mashape.com/cat=',
     success: function(response) {
@@ -42,11 +42,11 @@ $(".quote-text").animate({
 
   $(".quote-author").animate({
       opacity: 0
-    }, 500,
+    }, 200,
     function() {
       $(this).animate({
         opacity: 1
-      }, 500);
+      }, 200);
       $('#author').html(r.author);
     });
 
